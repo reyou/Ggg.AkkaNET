c9f289c976ad0f66dff451c5ff0413a8b5667434
aozdemir <aozdemir@webmd.net>
2019-01-15 18:00:52 -0500

    intro

---
+++ apps/app-docs/getakka.net/articles/intro/tutorial-1.html/intro1/intro/PrintMyActorRefActor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Akka.Actor;
+
+namespace intro
+{
+    public class PrintMyActorRefActor : UntypedActor
+    {
+        /// <summary>
+        /// To be implemented by concrete UntypedActor, this defines the behavior of the UntypedActor
+        /// </summary>
+        /// <param name="message"></param>
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case "printit":
+                    // Creates a new child actor of this context with the given name
+                    IActorRef secondRef = Context.ActorOf(Props.Empty, "second-actor");
+                    Console.WriteLine($"Second: {secondRef}");
+                    break;
+            }
+        }
+    }
+}
---
+++ apps/app-docs/getakka.net/articles/intro/tutorial-1.html/intro1/intro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Akka.Actor;
+using Akka.TestKit.NUnit;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestKit testKit = new TestKit();
+            //     An actor system is a hierarchical group of actors which share common
+            //     configuration, e.g. dispatchers, deployments, remote capabilities and
+            //     addresses. It is also the entry point for creating or looking up actors.
+            ActorSystem actorSystem = testKit.Sys;
+            // This class represents a configuration object used in creating an actor
+            Props props = Props.Create<PrintMyActorRefActor>();
+            IActorRef firstRef = actorSystem.ActorOf(props, "first-actor");
+            Console.WriteLine($"First: {firstRef}");
+            firstRef.Tell("printit", ActorRefs.NoSender);
+            Console.WriteLine("app is finished.");
+            Console.ReadLine();
+        }
+    }
+}
