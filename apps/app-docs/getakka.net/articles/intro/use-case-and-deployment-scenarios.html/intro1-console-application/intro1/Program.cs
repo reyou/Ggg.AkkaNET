eab359c1e4edf3abb957225eab845b4440f87a69
teoman shipahi <aytekoz@yahoo.com>
2019-07-18 16:52:58 -0400

    ASP.NET Creating the Akka.NET resources

---
+++ apps/app-docs/getakka.net/articles/intro/use-case-and-deployment-scenarios.html/intro1-console-application/intro1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Akka;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Configuration.Hocon;
+
+namespace intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (ActorSystem system = ActorSystem.Create("my-actor-server"))
+            {
+                // start two services
+                IActorRef service1 = system.ActorOf<Service1>("service1");
+                IActorRef service2 = system.ActorOf<Service2>("service2");
+            }
+            Console.WriteLine("App is finished.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ apps/app-docs/getakka.net/articles/intro/use-case-and-deployment-scenarios.html/intro1-console-application/intro1/Service1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using Akka.Actor;
+using Newtonsoft.Json;
+
+namespace intro1
+{
+    internal class Service1 : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(new
+            {
+                Method = $"{GetType().Name}.OnReceive",
+                Thread.CurrentThread.ManagedThreadId,
+                message
+            }, Formatting.Indented));
+        }
+    }
+}
