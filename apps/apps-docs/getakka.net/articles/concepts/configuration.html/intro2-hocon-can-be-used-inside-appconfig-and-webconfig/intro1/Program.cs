1bf4d9367e9484f5cd183b53909a4b13a478c57c
teoman shipahi <aytekoz@yahoo.com>
2019-07-23 15:16:47 -0400

    hocon

--- apps/apps-docs/getakka.net/articles/concepts/configuration.html/intro2-hocon-can-be-used-inside-appconfig-and-webconfig/intro1/Program.cs
+++ apps/apps-docs/getakka.net/articles/concepts/configuration.html/intro2-hocon-can-be-used-inside-appconfig-and-webconfig/intro1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace intro1
 {
@@ -10,7 +11,7 @@ namespace intro1
     {
         static void Main(string[] args)
         {
-            var system = ActorSystem.Create("MySystem"); //automatically loads App/Web.config
+            ActorSystem system = ActorSystem.Create("MySystem"); //automatically loads App/Web.config
 
         }
     }
